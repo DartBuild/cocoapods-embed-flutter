c6fdfcac64c7e57a4dc3d4ae2cb517bf745cb6b6
Soumya Mahunt <soumya.mahunt@tatadigital.com>
2022-03-09 22:23:39 +0530

    ci: setup CI/CD pipeline

---
+++ .github/config/config.js
@@ -0,0 +1,20 @@
+'use strict'
+const config = require('conventional-changelog-conventionalcommits');
+
+module.exports = config({
+    "types": [
+        { type: 'feat', section: '🚀 Features' },
+        { type: 'fix', section: '🐛 Fixes' },
+        { type: 'perf', section: '🐎 Performance Improvements' },
+        { type: 'revert', section: '⏪ Reverts' },
+        { type: 'build', section: '🛠 Build System' },
+        { type: 'ci', section: '💡 Continuous Integration' },
+        { type: 'refactor', section: '🔥 Refactorings' },
+        { type: 'doc', section: '📚 Documentation' },
+        { type: 'docs', section: '📚 Documentation' },
+        { type: 'style', section: '💄 Styles' },
+        { type: 'test', section: '✅ Tests' },
+        { type: 'wip', hidden: true },
+        { type: 'chore', hidden: true },
+    ]
+})
---
+++ .github/config/pre_changelog_hook.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const fs = require('fs');
+const semver = require('semver');
+const core = require('@actions/core');
+const childProcess = require("child_process");
+
+exports.preVersionGeneration = (version) => {
+  const { GITHUB_WORKSPACE } = process.env;
+  core.info(`Computed version bump: ${version}`);
+
+  const gem_info_file = path.join(GITHUB_WORKSPACE, 'lib/cocoapods-embed-flutter/gem_version.rb');
+  const gem_info = `${fs.readFileSync(gem_info_file)}`;
+  core.info(`Current gem info: ${gem_info}`);
+
+  currentVersion = gem_info.match(/VERSION\s*=\s'(.*)'/)[1];
+  core.info(`Current version: ${currentVersion}`);
+
+  if (semver.lt(version, currentVersion)) { version = currentVersion; }
+  core.info(`Final version: ${version}`);
+
+  const new_gem_info = gem_info.replace(/VERSION\s*=\s*.*/g, `VERSION = '${version}'.freeze`);
+  core.info(`Updated gem info: ${new_gem_info}`);
+  fs.writeFileSync(gem_info_file, new_gem_info);
+
+  const launchOption = { cwd: GITHUB_WORKSPACE };
+  childProcess.execSync('bundle exec rake demo', launchOption);
+  return version;
+}
+
+exports.preTagGeneration = (tag) => { }
